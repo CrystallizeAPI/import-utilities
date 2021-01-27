bc166ea2372a330b737a986a6bb4cee102ff17c1
Brendan Goodenough <brendan@goodenough.nz>
2021-01-27 11:20:04 +0100

    Rename numeric value -> number

--- src/types/shapes/components/numeric/numeric-content.input.ts
+++ src/types/shapes/components/numeric/numeric-content.input.ts
@@ -2,7 +2,7 @@ import { ComponentContentInput } from '../component-content.input'
 
 export interface NumericComponentContentInput extends ComponentContentInput {
   numeric: {
-    value: number
+    number: number
     unit?: string
   }
 }
