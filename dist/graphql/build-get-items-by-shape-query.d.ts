99aedc8b4146cfdb07029a1e995f89efec2bc02a
Brendan Goodenough <brendan@goodenough.nz>
2021-02-01 14:10:02 +0100

    Build

---
+++ dist/graphql/build-get-items-by-shape-query.d.ts
@@ -0,0 +1,1 @@
+export declare const buildGetItemsByShapeQuery: (id: string, language: string) => string;
---
+++ dist/graphql/build-get-items-by-shape-query.js
@@ -0,0 +1,109 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.buildGetItemsByShapeQuery = void 0;
+var json_to_graphql_query_1 = require("json-to-graphql-query");
+var buildGetItemsByShapeQuery = function (id, language) {
+    var query = {
+        query: {
+            shape: {
+                get: {
+                    __args: {
+                        id: id,
+                    },
+                    id: true,
+                    name: true,
+                    items: {
+                        __args: {
+                            language: language,
+                        },
+                        id: true,
+                        name: true,
+                        components: {
+                            componentId: true,
+                            __on: [
+                                {
+                                    __typeName: 'BooleanContent',
+                                    value: true,
+                                },
+                                {
+                                    __typeName: 'ComponentChoiceContent',
+                                    selectedComponent: {
+                                        componentId: true,
+                                        type: true,
+                                    },
+                                },
+                                {
+                                    __typeName: 'ContentChunkContent',
+                                    chunks: {
+                                        componentId: true,
+                                        type: true,
+                                    },
+                                },
+                                {
+                                    __typeName: 'DatetimeContent',
+                                    datetime: true,
+                                },
+                                {
+                                    __typeName: 'GridRelationsContent',
+                                    grids: {
+                                        id: true,
+                                    },
+                                },
+                                {
+                                    __typeName: 'ImageContent',
+                                    images: {
+                                        key: true,
+                                    },
+                                },
+                                {
+                                    __typeName: 'ItemRelationsContent',
+                                    items: {
+                                        id: true,
+                                    },
+                                },
+                                {
+                                    __typeName: 'LocationContent',
+                                    lat: true,
+                                    long: true,
+                                },
+                                {
+                                    __typeName: 'NumericContent',
+                                    value: true,
+                                    unit: true,
+                                },
+                                {
+                                    __typeName: 'PropertiesTableContent',
+                                    sections: {
+                                        title: true,
+                                        properties: {
+                                            key: true,
+                                            value: true,
+                                        },
+                                    },
+                                },
+                                {
+                                    __typeName: 'RichTextContent',
+                                    json: true,
+                                    html: true,
+                                },
+                                {
+                                    __typeName: 'SingleLineContent',
+                                    text: true,
+                                },
+                                {
+                                    __typeName: 'VideoContent',
+                                    videos: {
+                                        id: true,
+                                        title: true,
+                                    },
+                                },
+                            ],
+                        },
+                    },
+                },
+            },
+        },
+    };
+    return json_to_graphql_query_1.jsonToGraphQLQuery(query);
+};
+exports.buildGetItemsByShapeQuery = buildGetItemsByShapeQuery;
