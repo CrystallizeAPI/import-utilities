b95c32532815e48cda6a3e384647a34b967c076f
Brendan Goodenough <brendan@goodenough.nz>
2021-02-08 21:35:30 +0100

    Add identifier to shape type

--- dist/types/shapes/shape.d.ts
+++ dist/types/shapes/shape.d.ts
@@ -1,6 +1,6 @@
 import { Component } from './components/component';
 export interface Shape {
-    id: string;
+    identifier: string;
     name: string;
     type: string;
     components: Component[];
--- src/types/shapes/shape.ts
+++ src/types/shapes/shape.ts
@@ -1,7 +1,7 @@
 import { Component } from './components/component'
 
 export interface Shape {
-  id: string
+  identifier: string
   name: string
   type: string
   components: Component[]
