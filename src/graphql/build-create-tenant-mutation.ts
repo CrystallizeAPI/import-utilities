51279967ada14f17ecd3d4a4950c44d624a1fa29
Brendan Goodenough <brendan@goodenough.nz>
2021-03-10 11:57:15 +0100

    Add rootItemId to tenant type and fetch on create

--- src/graphql/build-create-tenant-mutation.ts
+++ src/graphql/build-create-tenant-mutation.ts
@@ -11,6 +11,7 @@ export const buildCreateTenantMutation = (input: TenantInput): string => {
           },
           id: true,
           identifier: true,
+          rootItemId: true,
           shapes: {
             identifier: true,
             name: true,
