8121023b26d0f5379da10cde6d701f8b6a824074
Håkon Krogh <hakonhk@gmail.com>
2021-07-21 09:56:49 +0200

    Specify the availabe component types

--- src/types/shapes/components/component.ts
+++ src/types/shapes/components/component.ts
@@ -1,7 +1,22 @@
 export interface Component {
   id: string
   name: string
-  type: string
+  type:
+    | 'boolean'
+    | 'componentChoice'
+    | 'contentChunk'
+    | 'datetime'
+    | 'gridRelations'
+    | 'images'
+    | 'itemRelations'
+    | 'location'
+    | 'numeric'
+    | 'paragraphCollection'
+    | 'propertiesTable'
+    | 'richText'
+    | 'selection'
+    | 'singleLine'
+    | 'videos'
   description?: string
   config?: any
 }
