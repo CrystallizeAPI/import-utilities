66b49f8e6187b1f0b81a6b50bad87d47da25882f
Brendan Goodenough <brendan@goodenough.nz>
2022-01-28 15:07:31 +0100

    Add basic order types

---
+++ src/bootstrap-tenant/json-spec/orders.ts
@@ -0,0 +1,55 @@
+export interface JSONAddress {
+  type?: 'delivery' | 'billing' | 'other'
+  firstName?: string
+  middleName?: string
+  lastName?: string
+  street?: string
+  street2?: string
+  streetNumber?: string
+  postalCode?: string
+  city?: string
+  state?: string
+  country?: string
+  phone?: string
+  email?: string
+}
+
+export interface JSONCustomer {
+  identifier?: string
+  firstName?: string
+  middleName?: string
+  lastName?: string
+  birthDate?: string
+  addresses?: JSONAddress[]
+  companyName?: string
+  taxNumber?: string
+}
+
+export interface JSONPrice {
+  currency: string
+  gross?: number
+  net?: number
+  // discounts
+  // tax
+}
+
+export interface JSONOrderItem {
+  name: string
+  sku?: string
+  productId?: string
+  productVariantId?: string
+  imageUrl?: string
+  quantity: number
+  price?: JSONPrice
+  subTotal?: JSONPrice
+  meta?: Record<string, unknown>
+  // subscription: JSONOrderItemSubscription
+  // subscriptionContractId?: string
+}
+
+export interface JSONOrder {
+  customer: JSONCustomer
+  cart: JSONOrderItem[]
+  // payment?: JSONPayment
+  total?: JSONPrice
+}
