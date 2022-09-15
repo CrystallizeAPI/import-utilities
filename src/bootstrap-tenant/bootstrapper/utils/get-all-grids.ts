ac04cd25711fe6157b37a9cfd21f97528ba3118c
Håkon Krogh <hakonhk@gmail.com>
2022-09-15 15:02:52 +0200

    Support multilingual for grid names

--- src/bootstrap-tenant/bootstrapper/utils/get-all-grids.ts
+++ src/bootstrap-tenant/bootstrapper/utils/get-all-grids.ts
@@ -33,11 +33,13 @@ export async function getAllGrids(
   language: string,
   context: BootstrapperContext
 ): Promise<JSONGrid[]> {
+  const tenantId = context.tenantId
+
   const response = await context.callPIM({
     query: QUERY,
     variables: {
       language,
-      tenantId: context.tenantId,
+      tenantId,
     },
   })
 
@@ -55,12 +57,57 @@ export async function getAllGrids(
     }
   }
 
-  function handleGrid(grid: any): JSONGrid {
+  async function handleGrid(grid: any): Promise<JSONGrid> {
+    // Get names for remaining languages
+    if (context.config.multilingual) {
+      grid.name = {
+        [language]: grid.name,
+      }
+
+      const remainingLanguages = context.languages
+        .map((l) => l.code)
+        .filter((l) => l !== language)
+      await Promise.all(
+        remainingLanguages.map(async (lang) => {
+          grid.name[lang] = await getNameForGrid(grid.id, lang, context)
+        })
+      )
+    }
     return {
       ...grid,
       rows: grid.rows?.map(handleRow) || [],
     }
   }
 
-  return response.data?.grid?.getMany?.map(handleGrid) || []
+  return Promise.all(response.data?.grid?.getMany?.map(handleGrid) || [])
+}
+
+async function getNameForGrid(
+  id: string,
+  language: string,
+  context: BootstrapperContext
+): Promise<string | null> {
+  const response = await context.callPIM({
+    query: `
+      query GET_NAME_FOR_GRID (
+        $id: ID!
+        $language: String!
+      ) {
+        grid {
+          get (
+            id: $id
+            language: $language
+          ) {
+            name
+          }
+        }
+      }
+    `,
+    variables: {
+      id,
+      language,
+    },
+  })
+
+  return response.data?.grid?.get?.name || null
 }
